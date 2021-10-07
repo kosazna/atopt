35b2fadf517e67c4f9017b39fc7e07e503b5859f
Kostas Aznavouridis <kaznavouridis@gmail.com>
2021-10-07 22:02:05 +0300

    (D)

--- core/cp3.py
+++ core/cp3.py
@@ -12,10 +12,8 @@ from pylab import rcParams
 rcParams['figure.figsize'] = 10, 4
 
 my_parser = argparse.ArgumentParser()
-my_parser.add_argument('-t', '--trips', action='store', type=int)
-my_parser.add_argument('-d', '--duties', action='store', type=int)
-my_parser.add_argument('-l', '--limit', action='store', type=int)
-
+my_parser.add_argument('-trips', action='store', type=int)
+my_parser.add_argument('-duties', action='store', type=int)
 
 args = my_parser.parse_args()
 
@@ -42,10 +40,11 @@ else:
     NDUTIES = args.duties
 
 # print(model.durations)
-# print(model.model.start_times)
+# print(model.start_times)
 
 
 sub = CpoModel(name="Pricing_Subproblem")
+break_states = ('prebreak', 'postbreak')
 
 min_start = model.data[start_time].min()
 max_start = model.data[start_time].max()
@@ -57,64 +56,66 @@ max_end = model.data[end_time].max()
 #                       size=trip.duration,
 #                       name=f'Trip_{idx}') for idx, trip in enumerate(model.trips)]
 
-# driving_time = {}
 
-# for d in range(NDUTIES):
-#     step = CpoStepFunction()
-#     step.set_value(0, max_end, 0)
-#     for t in range(len(model.trips)):
-#         step.set_value(model.model.start_times[t], model.model.end_times[t], 100)
-#     driving_time[d] = step
+# duties = [interval_var(start=(min_start, max_start),
+#                        end=(min_end, max_end),
+#                        size=(0, model.constraints.shift_span),
+#                        name=f"Duty_{i}",
+#                        optional=True)
+#           for i in range(NDUTIES)]
 
-duties = [interval_var(start=(min_start, max_start),
-                       end=(min_end, max_end),
-                       size=(0, model.constraints.total_driving),
-                       name=f"Duty_{i}",
-                       optional=True)
-          for i in range(NDUTIES)]
+driving_time = {}
+
+for d in range(NDUTIES):
+    for bs in break_states:
+        step = CpoStepFunction()
+        step.set_value(0, max_end, 0)
+        for t in range(len(model.trips)):
+            step.set_value(model.start_times[t], model.end_times[t], 100)
+        driving_time[(d, bs)] = step
+
+duties = {}
+for d in range(NDUTIES):
+    for bs in break_states:
+        duties[(d, bs)] = interval_var(start=(min_start, max_start),
+                                       end=(min_end, max_end),
+                                       size=(0, model.constraints.continuous_driving),
+                                       intensity=driving_time[(d, bs)],
+                                       name=f"Duty_{d}_{bs}",
+                                       optional=True)
 
 breaks = [interval_var(size=model.constraints.break_time,
                        name=f"BreakTime_{i}",
                        optional=True)
           for i in range(NDUTIES)]
 
-end_dt = []
-for t in range(NTRIPS):
-	end_dt.append([sub.integer_var(0, model.constraints.shift_span, "edt_"+str(t)+","+str(d)) for d in range(NDUTIES)])
-
 trip2duty = {}
 for t, trip in enumerate(model.trips):
     for d in range(NDUTIES):
-        trip2duty[(t, d)] = sub.interval_var(start=(trip.start_time, trip.start_time),
-                                             end=(trip.end_time, trip.end_time),
-                                             size=trip.duration,
-                                             name=f"Trip_{t:02} | Duty_{d:02}",
-                                             optional=True)
+        for bs in break_states:
+            trip2duty[(t, d, bs)] = sub.interval_var(start=(trip.start_time, trip.start_time),
+                                                     end=(trip.end_time,
+                                                          trip.end_time),
+                                                     size=trip.duration,
+                                                     name=f"Trip_{t:02} | Duty_{d:02} | {bs}",
+                                                     optional=True)
 
 ########################################
 
 for d in range(NDUTIES):
-    sub.add(sub.span(duties[d], [trip2duty[(t, d)] for t in range(NTRIPS)]))
+    sub.add(sub.span(duties[(d, bs)], [trip2duty[(t, d, bs)]
+            for t in range(NTRIPS) for bs in break_states]))
 
 for d in range(NDUTIES):
-    sub.add(sub.no_overlap([trip2duty[(t, d)] for t in range(NTRIPS)]))
+    sub.add(sub.no_overlap([trip2duty[(t, d, bs)]
+            for t in range(NTRIPS)for bs in break_states]))
 
 for t in range(NTRIPS):
-    sub.add(sub.sum([sub.presence_of(trip2duty[(t, d)])
-            for d in range(NDUTIES)]) == 1)
+    sub.add(sub.sum([sub.presence_of(trip2duty[(t, d, bs)])
+            for d in range(NDUTIES) for bs in break_states]) == 1)
 
-for t in range(NTRIPS):
-	previous_trips = []
-	for b in range(NTRIPS):
-		if model.end_times[b] <= model.start_times[t]:
-			previous_trips.append(b)
-	for d in range(NDUTIES):
-		sub.add(end_dt[t][d] == sum([(model.durations[b])*sub.presence_of(trip2duty[(b, d)]) for b in previous_trips]) + (model.durations[t]))
-
-for t in range(NTRIPS):
-	for d in range(NDUTIES):
-		sub.add(sub.if_then(sub.logical_and((end_dt[t][d] <= model.constraints.continuous_driving), (sub.presence_of(trip2duty[(t, d)]))), (sub.end_of(trip2duty[(t, d)]) <= sub.start_of(breaks[(d)]))))
-		sub.add(sub.if_then(sub.logical_and((end_dt[t][d] > model.constraints.continuous_driving), (sub.presence_of(trip2duty[(t, d)]))), (sub.start_of(trip2duty[(t, d)]) >= sub.end_of(breaks[(d)]))))
+for d in range(NDUTIES):
+    sub.add(sub.presence_of(duties[d, break_states[0]]) == sub.presence_of(duties[d, break_states[1]]))
 
 # cdt = {}
 
@@ -142,7 +143,7 @@ for t in range(NTRIPS):
 #     sub.add(tdt[d] <= model.constraints.total_driving)
 
 
-obj = sub.sum([sub.presence_of(duty) for duty in duties])
+obj = sub.sum([sub.presence_of(duties[d, break_states[0]]) for d in range(NDUTIES)])
 sub.add(sub.minimize(obj))
 
 if __name__ == "__main__":
@@ -157,17 +158,18 @@ if __name__ == "__main__":
     cpsol.print_solution()
 
     for d in range(NDUTIES):
-        if cpsol[duties[d]]:
-            print(f"\n> Duty {d} : {cpsol[duties[d]]}")
-            # print(cdt[d])
-            _tdt = 0
-            _ntrips = 0
-            for t in range(NTRIPS):
-                if cpsol[trip2duty[(t, d)]]:
-                    print(f"  - Trip {t} : {cpsol[trip2duty[(t, d)]]}")
-                    _tdt += model.durations[t]
-                    _ntrips += 1
-            print(f"\n  > Driving Time: {_tdt}, Trips: {_ntrips}")
+        for bs in break_states:
+            if cpsol[duties[(d, bs)]]:
+                print(f"\n> Duty {d} : {cpsol[duties[(d, bs)]]}")
+                # print(cdt[d])
+                _tdt = 0
+                _ntrips = 0
+                for t in range(NTRIPS):
+                    if cpsol[trip2duty[(t, d, bs)]]:
+                        print(f"  - Trip {t} : {cpsol[trip2duty[(t, d, bs)]]}")
+                        _tdt += model.durations[t]
+                        _ntrips += 1
+                print(f"\n  > Driving Time: {_tdt}, Trips: {_ntrips}")
 
             # print(cdt[d])
     cpsol.write(str(out))
@@ -176,9 +178,10 @@ if __name__ == "__main__":
         f"{date_str}_{status}_{bounds[0]}", origin=min_start, horizon=max_end)
 
     for d in range(NDUTIES):
-        if cpsol[duties[d]]:
-            visu.panel()
-            visu.sequence(name=duties[d].get_name(),
-                          intervals=[(cpsol.get_var_solution(trip2duty[(t, d)]), d, str(t)) for t in range(NTRIPS) if cpsol[trip2duty[(t, d)]]])
+        for bs in break_states:
+            if cpsol[duties[(d, bs)]]:
+                visu.panel()
+                visu.sequence(name=duties[d].get_name(),
+                            intervals=[(cpsol.get_var_solution(trip2duty[(t, d, bs)]), d, str(t)) for t in range(NTRIPS) if cpsol[trip2duty[(t, d, bs)]]])
 
     visu.show()
